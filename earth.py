39c4b9b8145573d10a48adf6b0f9ff95a5b2804c
catamontaldo <cmontaldo@mail.austral.edu.ar>
2025-03-28 12:54:16 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -5,4 +5,3 @@ def earth():
     d = x > y
     print( f"The result of {x} comes first in the diccionary than {y} is" , c)
     print( f"The result of {x} comes first in the diccionary tan {y} is" , d)
-earth()
